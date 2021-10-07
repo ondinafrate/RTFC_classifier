8e8d0d77c88533974ca2942a2c732ae79c612e8e
Ondina Frate <ondina@ondinas-mbp.lan>
2021-10-06 20:05:13 -0700

    adding connections

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const conversationsList = [1483, 1484, 1466, 1465, 1461, 1462, 1463, 1447, 1446, 1438, 1441, 1478, 1442, 1434, 1464, 1414, 1413, 1412, 1407, 1403, 1406, 1382, 1379, 1380, 1378, 1411, 1405, 1363, 1360, 1439, 1358, 1355, 1353, 1357, 1352, 1347, 1348, 1349, 1354, 1320, 1323, 1322, 1317, 1304, 1319, 1306, 1311, 1309, 1336, 1310, 1305, 1321, 1293, 1312, 1307, 1290, 1292, 1281, 1280, 1273, 1285, 1268, 1269, 1282, 1251, 1219, 1211, 1225, 1227, 1284, 1223, 1314, 1231, 1192, 1286, 1191, 1190, 1220, 1230, 1193, 1183, 1184, 1189, 1221, 1179, 1177, 1182, 1175, 1178, 1171, 1172, 1174, 1181, 1173, 1170, 1222, 1160, 1337, 1161, 1162, 1157, 1159, 1154, 1151, 1152, 1141, 1131, 1122, 1123];
 
+const canvasHeight = 650;
+
 const width = 1130;
 const height = 600;
+
+document.getElementById("chart").style.height = "650px";
 
 let svg = d3.select("#chart")
     .append("svg")
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", canvasHeight);
 
 let g = svg.append('g');
 
@@ -77,7 +81,7 @@ function drawConversation({
     snippets.forEach((snippet, index) => {
         g.append('rect')
             .attr('class', () => {
-                return "snippet " + tags[Math.floor(Math.random() * tags.length * 2)]
+                return "snippet " + tags[Math.floor(Math.random() * tags.length * 10)]
             })
             .attr('x', x + conversationPadding + scale(snippet['audio_start_offset']))
             .attr('y', y + conversationPadding + ((snippetHeight + verticalSnippetPadding) * Number(snippet.speaker_id)))
@@ -140,11 +144,44 @@ for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', toggleSnippet, false);
 }
 
+var link = d3.linkHorizontal()
+    .x(function (d) {
+        return d.x;
+    })
+    .y(function (d) {
+        return d.y;
+    });
+
 function toggleSnippet(e) {
     this.classList.toggle('active');
     const id = e.target.id;
+    d3.selectAll('.' + id).each(function (d, i) {
+        let x = d3.select(this).attr('x');
+        let y = d3.select(this).attr('y');
+
+        let indexOfTag = tags.indexOf(id);
+        let tagWidth = width / tags.length;
+
+        let data = {
+            source: {
+                x: tagWidth * indexOfTag + tagWidth / 2,
+                y: canvasHeight
+            },
+            target: {
+                x,
+                y
+            }
+        };
+
+        g.append('path')
+            .attr("d", link(data))
+            .attr("class", "line line_" + id)
+            .style("stroke-width", 0.5)
+            .attr('fill', 'none');
+    })
     if (highlightedSnippets.has(id)) {
         highlightedSnippets.delete(id)
+        d3.selectAll('.line_' + id).remove();
         d3.selectAll('.' + id).classed('highlight', false);
     } else {
         highlightedSnippets.add(id);
@@ -178,7 +215,7 @@ const zoom = d3.zoom()
     .scaleExtent([1, 10])
     .translateExtent([
         [0, 0],
-        [width, height]
+        [width, canvasHeight]
     ])
     .on('zoom', handleZoom);
 
